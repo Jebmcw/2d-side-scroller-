e53617b7c99be1afafaa92c0090298d9e942298d
jebmcw03 <142943526+jebmcw03@users.noreply.github.com>
2024-02-21 20:34:25 -0600

    Added main.py and level1.py

---
+++ Main/levels/level1.py
@@ -0,0 +1,22 @@
+# levels/level1.py
+import pygame
+
+class Level1:
+    def __init__(self, display, gameStateManager):
+        self.display = display
+        self.gameStateManager = gameStateManager
+        
+
+    def run(self):
+        
+        self.display.fill((0, 0, 255))
+        
+
+
+
+
+
+
+
+
+
