5c3182e2f7eed10c022072e966289b6322d58687
Blake Woods <yew3@txstate.edu>
2024-03-29 17:43:58 -0500

    Added the boss at the end of the game

--- Main/levels/level1.py
+++ Main/levels/level1.py
@@ -4,7 +4,7 @@ import time
 from levels.mobs.mobs_LevelOne import Mob
 from main_character.player import Player
 from levels.backgrounds.background import Background
-
+from levels.Level1_boss.level1_boss import Boss
 
 class Level1:
     def __init__(self, display, gameStateManager):
@@ -12,12 +12,13 @@ class Level1:
         self.gameStateManager = gameStateManager
         
         self.mobs = pygame.sprite.Group()  # Corrected from a list to a sprite group
+        self.boss = pygame.sprite.Group()
         
         # Initialize Background with the display
         self.bg = Background(self.display)
         
         # Frame rate and timing for spawns
-        self.FPS = 25
+        self.FPS = 45
         self.start_time = time.time()
         self.spawn_intervals = [1, 5, 9, 12, 14] # seconds between spawns
         self.next_spawn_time = self.spawn_intervals[0]
@@ -37,10 +38,9 @@ class Level1:
         if self.spawn_intervals[self.spawn_index] ==1:
             self.some_additional_offset = 500
             dynamic_offset = self.bg.scroll + self.some_additional_offset
-            mobs_to_add = Mob.spawn_mobs_horizontally(self.display, 1, 500, 50, dynamic_offset)
+            mobs_to_add = Mob.spawn_mobs_horizontally(self.display, 1, 400, 50, dynamic_offset)
             self.mobs.add(*mobs_to_add)
             print("Mob spawned : 1")
-    
         if self.spawn_intervals[self.spawn_index] == 5:
             self.some_additional_offset = 500
             dynamic_offset = self.bg.scroll + self.some_additional_offset
@@ -68,7 +68,14 @@ class Level1:
             mobs_to_add = Mob.spawn_mobs_horizontally(self.display, 1, 500, 50, dynamic_offset)
             self.mobs.add(*mobs_to_add)
             print("Mob spawned : 14")
-                
+            
+    def spawn_boss(self):
+        if self.bg.scroll == 8000:
+            dynamic_offset = 10000
+            boss_to_add = Boss.spawn_boss_horizontally(self.display, 1, 500, 50, dynamic_offset)
+            self.boss.add(boss_to_add)
+            print("boss spawned")
+               
     def run(self):
         current_time = time.time()
         elapsed_time = current_time - self.start_time
@@ -105,8 +112,12 @@ class Level1:
             mob_world_x = mob.rect.x - self.bg.scroll
             self.display.blit(mob.image, (mob_world_x, mob.rect.y))
             Mob.draw_health_bar(self.display, mob, self.bg.scroll)
+         
+        self.spawn_boss()   
+        self.boss.update()
+        for boss in self.boss:
+            self.display.blit(boss.image, (boss.rect.x-self.bg.scroll, boss.rect.y))
             
-       
         #Update and draw player
         if self.jump == 1:
             self.mainCharacter.update()
