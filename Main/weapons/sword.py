d9f2f4c67320927ce28405121f9499c2ff0a5fd1
Blake Woods <yew3@txstate.edu>
2024-04-14 18:58:32 -0500

    added collisions between the sword and mob and boss

--- Main/weapons/sword.py
+++ Main/weapons/sword.py
@@ -12,56 +12,46 @@ class Sword():
         self.scaled_height =90  # Desired height after scaling
         
         self.sword_img1 = pygame.transform.scale(self.sword_img, (self.scaled_width, self.scaled_height))
-        self.sword_img1 = pygame.transform.rotate(self.sword_img1, 270)
-        self.sword_rect = self.sword_img.get_rect()
-        print('Sword dimensions: ', self.sword_rect.width, ',', self.sword_rect.height)
+        self.sword_img1 = pygame.transform.rotate(self.sword_img1, 280)
+        self.rect = self.sword_img1.get_rect()
         
         
+        print('Sword dimensions: ', self.rect.width, ',', self.rect.height)
         self.sword_img2 = pygame.transform.rotate(self.sword_img1, 330)
-        self.sword_img3 = pygame.transform.rotate(self.sword_img1, 315)
-        self.sword_img4 = pygame.transform.rotate(self.sword_img1, 300)
-        self.sword_img5 = pygame.transform.rotate(self.sword_img1,315)
-        self.sword_img6 = pygame.transform.rotate(self.sword_img1,330)
+        self.sword_img3 = pygame.transform.rotate(self.sword_img2, 30)
+        
+        
+        self.sword_img4 = pygame.transform.rotate(self.sword_img1, 320)
+        self.sword_img5 = pygame.transform.rotate(self.sword_img1,320)
+        self.sword_img6 = pygame.transform.rotate(self.sword_img1,320)
+        
         self.SwordFrame = 0
         
-        
-   
-    
-    
-    
     def update(self,keys):
-        sword_x_offset = -10  # Adjust so the sword appears correctly relative to Freddy
-        sword_y_offset = 20  # Adjust based on your game's needs
-    
-        sword_position = (self.freddy.rect.x + sword_x_offset, self.freddy.rect.y + sword_y_offset)
+        sword_x_offset = -15  # Adjust so the sword appears correctly relative to Freddy
+        sword_y_offset = 10  # Adjust based on your game's needs
         
+        self.rect.x = self.freddy.rect.x + sword_x_offset
+        self.rect.y = self.freddy.rect.y + sword_y_offset
        
-       
-        
         if keys[pygame.K_LSHIFT]:
             if self.SwordFrame <= 20:
-                self.display.blit(self.sword_img1, sword_position)
-                self.SwordFrame += 4
-            elif self.SwordFrame <= 40:
-                self.display.blit(self.sword_img2, sword_position)
-                self.SwordFrame += 4
-            elif self.SwordFrame <= 60:
-                self.display.blit(self.sword_img3, sword_position)
-                self.SwordFrame += 4
-            elif self.SwordFrame <=80:
-                #self.SwordFrame +=2
-                self.display.blit(self.sword_img4, sword_position)
-                self.SwordFrame += 4
-                #if self.SwordFrame == 80:
-                   # self.SwordFrame = 0
-            elif self.SwordFrame <=100:
-                self.display.blit(self.sword_img5, sword_position)
-                self.SwordFrame +=4
+                self.display.blit(self.sword_img1, (self.rect.x, self.rect.y))
+                self.SwordFrame += 1
+            elif self.SwordFrame <=40:
+                self.display.blit(self.sword_img2, (self.rect.x, self.rect.y))
+                self.SwordFrame +=1 
             else:
-                self.SwordFrame +=4
-                self.display.blit(self.sword_img6, sword_position)
-                if self.SwordFrame == 120:
-                    self.SwordFrame =0
+                sword_x_offset = -30
+                sword_y_offset = -30
+                self.rect.x = self.freddy.rect.x + sword_x_offset
+                self.rect.y = self.freddy.rect.y + sword_y_offset
+                self.SwordFrame +=1
+                self.display.blit(self.sword_img3, (self.rect.x, self.rect.y))
+                if self.SwordFrame == 60:
+                    self.SwordFrame =21
+           
+                
             
             
                 
